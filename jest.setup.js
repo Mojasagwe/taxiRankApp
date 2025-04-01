61efd70fe7f4acc1c5842dd54d0c7bea38595878
Thato <exah@Thato-MacBook-Pro.local>
2025-04-01 09:25:48 +0200

    Fix NativeAnimatedHelper mock in Jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -5,6 +5,16 @@ const { jest } = require('@jest/globals');
 jest.mock('react-native-dropdown-picker', () => require('./__mocks__/react-native-dropdown-picker'));
 
 // Silence the warning: Animated: `useNativeDriver` is not supported
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => ({
+  __esModule: true,
+  default: {
+    extractNativeEvent: jest.fn(),
+    API: {
+      createAnimatedComponent: jest.fn(),
+    },
+    addListener: jest.fn(),
+    removeListeners: jest.fn(),
+  },
+}), { virtual: true });
 
 // Mock any other problematic libraries as needed 
