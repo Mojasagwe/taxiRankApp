96ab9bac2ab1f272e82aeacda093fa6ec0ec318b
Thato <exah@Thato-MacBook-Pro.local>
2025-04-01 09:23:00 +0200

    Fix Jest setup file by using @jest/globals package

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,10 @@
 // Jest setup file
+const { jest } = require('@jest/globals');
 
 // Mock react-native-dropdown-picker
-global.jest.mock('react-native-dropdown-picker', () => require('./__mocks__/react-native-dropdown-picker'));
+jest.mock('react-native-dropdown-picker', () => require('./__mocks__/react-native-dropdown-picker'));
 
 // Silence the warning: Animated: `useNativeDriver` is not supported
-global.jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 // Mock any other problematic libraries as needed 
